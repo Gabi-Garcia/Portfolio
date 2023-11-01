fe983613a1aa4b6367ea0f1c844e57ba0dac8be6
Gabi-Garcia <96738373+Gabi-Garcia@users.noreply.github.com>
2023-11-01 14:49:10 +0100

    cambios varios:style,public,cards

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,7 @@
  import {Button} from "../Button/Button";
 
  export const Footer = () => `
-<h2>Contactame</h2>
+<h2>Contáctame</h2>
 <div>
 ${Button("/github.png", "GitHub", "https://github.com/Gabi-Garcia")}
 ${Button("/linkedin.png", "Linkedin", "https://www.linkedin.com/in/gabriel-garcia-ab029b210/")}
--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -6,15 +6,19 @@ export const Home = () => {
     cleanPage(main);
     main.innerHTML = `
     <section class="home">
-    <p class="holaSoy">Hola, soy </p>
-    <h1>Gabriel Garcia</h1>
-    <p class="mePresento">Me presento como un programador full stack junior altamente
-    motivado y apasionado por la creación de soluciones tecnológicas
-    innovadoras. Me gustaría compartir con ustedes mis habilidades y
-    experiencias en el campo del desarrollo web y contribuir a su equipo.
-    </p>
-   
-    <a href="mailto:gabriel.garcia.development@gmail.com">Puedes escribirme aquí 📧</a>
+    <p class="holaSoy">Hola, soy</p>
+            <div class="cajaArriba">
+                <h1>Gabriel Garcia</h1>
+                <div class="cajaFoto">
+                    <img src="/perfil.jpg">
+                </div>
+            </div>
+            <p class="mePresento">Me presento como un programador full stack junior altamente
+            motivado y apasionado por la creación de soluciones tecnológicas
+            innovadoras. Me gustaría compartir con ustedes mis habilidades y
+            experiencias en el campo del desarrollo web y contribuir a su equipo.
+            </p>
+            <a href="mailto:gabriel.garcia.development@gmail.com">Contrátame</a>
     </section>`;
 };
     
